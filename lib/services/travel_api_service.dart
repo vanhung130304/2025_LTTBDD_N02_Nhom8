1fa1d78b6d051988dced23ecfd565aa5eb86297d
vanhung130304 <hungvan13032004@gmailcom.comcom>
2025-11-04 23:09:46 +0700

    sửa api
    :wq

--- lib/services/travel_api_service.dart
+++ lib/services/travel_api_service.dart
@@ -2,41 +2,35 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class TravelApiService {
-  static const String _baseUrl = 'https://travel-places.p.rapidapi.com/';
+  static const String _baseUrl =
+      'https://travel-guide-api-city-guide-top-places.p.rapidapi.com/check?noqueue=1';
+
   static const Map<String, String> _headers = {
     'Content-Type': 'application/json',
-    'X-RapidAPI-Key': 'd74e049739msha519ef88ebcde2fp10a123jsn568df6a628e6',
-    'X-RapidAPI-Host': 'travel-places.p.rapidapi.com',
+    'X-RapidAPI-Key': '79005bb626msh69464491d30ab1cp188d36jsn2748d10431f9',
+    'X-RapidAPI-Host': 'travel-guide-api-city-guide-top-places.p.rapidapi.com',
   };
 
-  static Future<List<dynamic>> fetchPlaces() async {
-    const String query = '''
-    {
-      getPlaces(
-        lat: 16.0471,
-        lng: 108.2068,
-        maxDistMeters: 30000
-      ) {
-        name
-        lat
-        lng
-        abstract
-        distance
-        categories
-      }
-    }
-    ''';
+  static Future<List<dynamic>> fetchPlaces({
+    String region = "London",
+    String language = "en",
+    List<String> interests = const ["historical", "cultural", "food"],
+  }) async {
+    final body = json.encode({
+      "region": region,
+      "language": language,
+      "interests": interests,
+    });
 
     final response = await http.post(
-      Uri.parse('${_baseUrl}'),
+      Uri.parse(_baseUrl),
       headers: _headers,
-      body: json.encode({'query': query}),
+      body: body,
     );
 
     if (response.statusCode == 200) {
       final jsonBody = json.decode(response.body);
-      final places = jsonBody['data']['getPlaces'];
-      return places ?? [];
+      return jsonBody['data'] ?? jsonBody['places'] ?? [];
     } else {
       throw Exception('Không thể tải dữ liệu: ${response.statusCode}');
     }
